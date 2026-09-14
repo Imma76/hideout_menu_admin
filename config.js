5c9534e8c12b1a12fcba6770f9a8497176180ffa
Emmanuel Ugwueze <emmanuelugwueze6@gmail.com>
2026-09-14 20:05:40 +0100

    update url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // Point this at wherever the backend API is running/deployed.
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = "https://hideout-menu-backend-fbea06161e33.herokuapp.com";
+// 'http://localhost:3000';
 
 // The public menu site the QR code should link to.
-const FRONTEND_URL = 'http://localhost:5500';
+const FRONTEND_URL = "http://localhost:5500";
